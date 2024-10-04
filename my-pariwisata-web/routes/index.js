bc0c50e0f36c23631f2c0736283fdb7269650d7e
krisnaRiyandi <krisnariyandi_2226240155@mhs.mdp.ac.id>
2024-10-04 11:14:53 +0700

    UPDATE : penyelesaian my-pariwisata-web

--- my-pariwisata-web/routes/index.js
+++ my-pariwisata-web/routes/index.js
@@ -15,6 +15,28 @@ router.get('/orderpaket', function(req, res, next) {
 });
 
 router.post('/orderdetail', function(req, res, next) {
-  res.render('orderdetail', { title: 'Detail Pemesanan Paket' });
-});
+    const orderData = {
+      nama: req.body.nama,
+      telepon: req.body.telepon,
+      email: req.body.email,
+      alamat: req.body.alamat,
+      paket: req.body.paket,
+      jumlah: req.body.jumlah,
+      tanggal: req.body.tanggal,
+      durasi: req.body.durasi,
+      tujuan: req.body.tujuan,
+      panduan: req.body.panduan,
+      permintaan: req.body.permintaan,
+      transportasi: req.body.transportasi,
+      akomodasi: req.body.akomodasi,
+      tipekamar: req.body.tipekamar,
+      pembayaran: req.body.pembayaran,
+      
+  };
+
+    // Redirect ke halaman detail order dengan data order
+    res.render('orderdetail', {title: 'Detail Pemesanan Paket', orderData });
+  });
+//   res.render('orderdetail', { title: 'Detail Pemesanan Paket' });  
+// });
 module.exports = router;
